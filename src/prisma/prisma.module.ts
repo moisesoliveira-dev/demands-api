a91ef7caae542d579123f0e0c398bcc44702ff15
moises.oliveira <mduarte@digiboard.com.br>
2026-04-30 15:49:54 -0400

    mudança no module do prisma

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,10 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service.js';
-import { SeedService } from './seed.service.js';
 
 @Global()
 @Module({
-    providers: [PrismaService, SeedService],
+    providers: [PrismaService],
     exports: [PrismaService],
 })
 export class PrismaModule { }
